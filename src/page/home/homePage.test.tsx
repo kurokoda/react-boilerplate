e9f87391cee1422c83d3b9031f4652c95b5a6605
Ian Greenough <ian@sutra.fit>
2021-10-28 15:16:28 -0500

    [CRA-1] Create React App Boilerplate

--- src/page/home/homePage.test.tsx
+++ src/page/home/homePage.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { HomePage } from './homePage';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<HomePage />);
+  const linkElement = screen.getByText(/Welcome to the home page!/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/page/home/homePage.tsx
@@ -0,0 +1,14 @@
+import "./homePage.css";
+import React from "react";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <div className="homepage">
+      <header className="homepage-header">
+        <p>Welcome to the home page!</p>
+      </header>
+    </div>
+  );
+};
+
+export { HomePage };
